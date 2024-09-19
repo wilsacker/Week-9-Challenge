ea445ff0635dd7fb6bb861d1ed56c954c07b806c
wilsacker <williamsuttona@gmail.com>
2024-09-19 16:02:16 -0700

    added array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -2,8 +2,18 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    { type: 'input', name: 'title', message: 'What is the title of your project?' },
+    { type: 'input', name: 'description', message: 'Provide a short description of your project:' },
+    { type: 'input', name: 'installation', message: 'How do you install your project?' },
+    { type: 'input', name: 'usage', message: 'Provide instructions for using the project:' },
+    { type: 'list', name: 'license', message: 'Choose a license for your project:', choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'] },
+    { type: 'input', name: 'contributing', message: 'How can others contribute to your project?' },
+    { type: 'input', name: 'tests', message: 'How can users run tests?' },
+    { type: 'input', name: 'github', message: 'Enter your GitHub username:' },
+    { type: 'input', name: 'email', message: 'Enter your email address:' }
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
