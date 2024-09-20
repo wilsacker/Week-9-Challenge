eb7a8330da288f4ffd3da0b4f7b8eda3af0dda90
wilsacker <williamsuttona@gmail.com>
2024-09-19 22:48:48 -0700

    finished index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // Array of questions for user input
 const questions = [
@@ -15,7 +16,7 @@ const questions = [
     { type: 'input', name: 'email', message: 'Enter your email address:' }
   ];
 
-// Function to write README file
+// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
         if (err) {
@@ -24,8 +25,13 @@ function writeToFile(fileName, data) {
     })
 }
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+      const readmeContent = generateMarkdown(answers);
+      writeToFile('README.md', readmeContent);
+    });
+}
 
 // Function call to initialize app
 init();
