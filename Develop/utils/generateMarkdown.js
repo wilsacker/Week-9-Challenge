02e8c991139ee226fe213540c469102dc100742a
wilsacker <williamsuttona@gmail.com>
2024-09-19 22:56:31 -0700

    generateMarkdown js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,11 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `![License](https://img.shields.io/badge/License-${license.replace(' ', '%20')}-blue.svg)`;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return '';
+  }
+  
+  switch (license) {
+    case 'MIT':
+      return '[MIT](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
+    case 'GPL 3.0':
+      return '[GPL 3.0](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'BSD 3':
+      return '[BSD 3](https://opensource.org/licenses/BSD-3-Clause)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
