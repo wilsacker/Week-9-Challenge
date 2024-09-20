ddaa8cc3de80f057b70f8336353abe8616ebf4d9
wilsacker <williamsuttona@gmail.com>
2024-09-19 23:05:29 -0700

    completed js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'None') {
@@ -7,36 +7,64 @@ function renderLicenseBadge(license) {
   return `![License](https://img.shields.io/badge/License-${license.replace(' ', '%20')}-blue.svg)`;
 }
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+}
+
+// Function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
   if (license === 'None') {
     return '';
   }
-  
-  switch (license) {
-    case 'MIT':
-      return '[MIT](https://opensource.org/licenses/MIT)';
-    case 'Apache 2.0':
-      return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
-    case 'GPL 3.0':
-      return '[GPL 3.0](https://www.gnu.org/licenses/gpl-3.0)';
-    case 'BSD 3':
-      return '[BSD 3](https://opensource.org/licenses/BSD-3-Clause)';
-    default:
-      return '';
-  }
+  return `## License\n\nThis project is licensed under the ${license} license.\n`;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+${renderLicenseLink(data.license)}  <!-- Include the license in the TOC -->
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}  <!-- The actual license section -->
+
+## Contributing
+${data.contributing}
+
+## Tests
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions
+If you have any questions, feel free to reach out:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
